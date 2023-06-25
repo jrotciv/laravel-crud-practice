44898adb32d2a9c9401f5d561f8fbed53a8900d4
Joao Victor Souza <jvsouzasantos@id.uff.br>
2023-06-24 23:43:55 -0300

    feat(forum): listando todos os topicos

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,2 +1,26 @@
-<h1>Couve-flor</h1>
+<h1>Tópicos</h1>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
+
 <a href="{{ route('forum.create')}}">Criar tópico</a>
