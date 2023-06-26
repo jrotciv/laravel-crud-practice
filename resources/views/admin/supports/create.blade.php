0b62a14c0f76ad693abf460669009228a4566629
Joao Victor Souza <jvsouzasantos@id.uff.br>
2023-06-26 01:54:47 -0300

    feat: aplicar compnents para views

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,15 +1,8 @@
 <h1>Novo tópico</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('forum.store') }}" method="post">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
-    @csrf()
-    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
-    <textarea name="body" cols="30" rows="10" placeholder="Descrição">{{ old('body') }}</textarea>
-    <input type="submit" value="Enviar">
+    @include('admin.supports.partials.form')
 </form>
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,16 +1,11 @@
 <h1>Editar tópico {{ $support->id }}</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert/>
 
 <form action="{{ route('forum.update', $support->id) }}" method="post">
     {{-- <input type="hidden" value="{{ csrf_token() }}" name="_token"> --}}
-    @csrf()
     @method('put')
-    <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
-    <textarea name="body" cols="30" rows="10" placeholder="Descrição">{{ $support->body }}</textarea>
-    <input type="submit" value="Enviar">
+    @include('admin.supports.partials.form', [
+        'support'=> $support
+    ])
 </form>
