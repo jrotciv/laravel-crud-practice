edb198c3d278df9dc048b5f7db90667faf968c3b
Joao Victor Souza <jvsouzasantos@id.uff.br>
2023-06-28 00:18:25 -0300

    feat(api): buscar topico

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\StoreUpdateSupport;
 use App\Http\Resources\SupportResource;
 use App\Services\SupportService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SupportController extends Controller
 {
@@ -38,7 +39,13 @@ class SupportController extends Controller
      */
     public function show(string $id)
     {
-        //
+        if (!$support = $this->service->findOne($id)){
+            return response()->json([
+                'error' => "Not found"
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return new SupportResource($support);
     }
 
     /**
